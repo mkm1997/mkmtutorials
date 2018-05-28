bdc70d2bdb963b79225dc2a3ce33f4d9c09aa9e1
Manish kumar Mishra <mmanish184@gmail.com>
2018-05-28 12:44:05 +0530

    Update settings.py

--- mkmtutorials/mkmtutorials/settings.py
+++ mkmtutorials/mkmtutorials/settings.py
@@ -20,7 +20,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'mmanish184@gmail.com'
-EMAIL_HOST_PASSWORD = 'M@anish1997'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 
 
